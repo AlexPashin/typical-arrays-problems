8a199b5f4208c7170ea9753a085f83f4b895462c
Alex Pashin <71001542+AlexPashin@users.noreply.github.com>
2020-10-02 15:52:07 +0400

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let min = Number.MAX_VALUE;
+    for (let i = 0; i < array.length; i++)
+        if (array[i] < min) min = array[i];
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let max = Number.MIN_VALUE;
+    for (let i = 0; i < array.length; i++)
+        if (array[i] > max) max = array[i];
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let avg = 0;
+    for (let i = 0; i < array.length; i++)
+        avg += array[i];
+    return avg / array.length;
 }
